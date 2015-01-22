d21c2450c3866fa5d0e817d1c140ecfda41700ae
saurabhJaju <saurabh.jaju2@gmail.com>
2015-01-22 23:01:26 +0530

    Rprogramming Assign 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,9 +1,22 @@
-## Put comments here that give an overall description of what your
-## functions do
+## create and store a matrix using matrix function in an object
+## pass the object to makeCacheMatrix function
+## and get output in another object
+##now pass this resultant object to the cacheSolve function
 
-## Write a short comment describing this function
+## 
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv<- NULL     #default value
+        set<- function(y){
+                x <<- y    # cached input matrix
+                inv <<- NULL #default value in cache
+        }
+        get<- function() x      
+        setinv <- function(solve) inv <<- solve  #solve function passed 
+        getinv <- function() inv
+        list(set = set, get = get, setinv = setinv, getinv = getinv)
+        # creates a list to acommodate the functions
+        
 
 }
 
@@ -11,5 +24,14 @@ makeCacheMatrix <- function(x = matrix()) {
 ## Write a short comment describing this function
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)){   #checks if inv has previous data
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get() #get function to get input matrix
+        inv<- solve(data,...) #solve function applied to input matrix
+        x$setinv(inv)
+        inv
+       
 }
